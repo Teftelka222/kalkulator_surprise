003ed118ad88da8d3527fcf7ae5525952cb6fbe9
Teftelka222 <skulptordewid@gmail.com>
2025-04-06 00:28:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+    
+    // This will replace the result of any mathematical operation with "I love you❤️💌💞"
+    if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
+        display.value = 'I love you❤️💌💞';
+    } else {
+        try {
+            display.value = eval(expression);
+        } catch {
+            display.value = 'Error';
+        }
+    }
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
